eae228b10235b02bd8e43f80fd430cfcaf6b222d
Gabriel <gab.val.and.rod@gmail.com>
2026-04-02 11:14:15 -0300

    "API v2.0"

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get, Post, Body } from '@nestjs/common';
 import { UsersService } from './users.service';
 
-@Controller('users') // 👉 rota: /users
+@Controller('users')
 export class UsersController {
   constructor(private usersService: UsersService) {}
 

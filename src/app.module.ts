662e44a5939d40b14277bee0a4c94e5ae49440b8
Gabriel <gab.val.and.rod@gmail.com>
2026-04-02 14:24:51 -0300

    "API v4.0"

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { PrismaModule } from './prisma/prisma.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [PrismaModule, UsersModule],
+  // eslint-disable-next-line prettier/prettier
+  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule, UsersModule],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
+  app.useGlobalFilters({
+    catch(exception) {
+      console.error(exception);
+      throw exception;
+    },
+  });
 }
 bootstrap();
